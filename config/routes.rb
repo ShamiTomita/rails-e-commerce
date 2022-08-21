af3ed3c401bc53074e0328c78e28729b838c6644
ShamiTomita <shamitomita@gmail.com>
2022-08-21 15:08:48 -0400

    devise login signup logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :orders
   resources :products
   devise_for :users
-   root 'public#index'
+  devise_scope :user do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+end
+  root 'public#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
