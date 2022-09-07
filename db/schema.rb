c38cec0db8dfad966bf9136ad02cf1897009efc3
ShamiTomita <shamitomita@gmail.com>
2022-09-07 16:56:12 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_21_185520) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_07_204639) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "cart_id"
+    t.integer "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "quantity"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "product_id"
     t.decimal "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "payment_method"
   end
 
   create_table "products", force: :cascade do |t|
