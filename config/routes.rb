6cceeedffe22a542d124bc06491d8016f3b1c3e4
ShamiTomita <shamitomita@gmail.com>
2022-09-07 16:56:17 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+  get 'carts/:id' => "carts#show", as: "cart"
+  delete 'carts/:id' => "carts#destroy"
+
+  post 'line_items/:id/add' => "line_items#add_quantity", as: "line_item_add"
+  post 'line_items/:id/reduce' => "line_items#reduce_quantity", as: "line_item_reduce"
+  post 'line_items' => "line_items#create"
+  get 'line_items/:id' => "line_items#show", as: "line_item"
+  delete 'line_items/:id' => "line_items#destroy"
+
   resources :orders
   resources :products
   devise_for :users
   devise_scope :user do
-  get '/users/sign_out' => 'devise/sessions#destroy'
-end
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   root 'public#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
