fbe4e5b3cb5c6a78b7fbfaae5aa2ca1b52090129
ShamiTomita <shamitomita@gmail.com>
2022-09-07 18:48:49 -0400

    carts line carts

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,10 @@
 class LineItem < ApplicationRecord
-  belongs_to :product 
+  belongs_to :product
   belongs_to :cart
   belongs_to :order
+
+  def total_price
+    self.quantity * self.product.price
+  end
+  
 end
