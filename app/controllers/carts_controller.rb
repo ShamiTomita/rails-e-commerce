5c024b82dad18ae27243bc40a667987ba1886595
ShamiTomita <shamitomita@gmail.com>
2022-09-11 14:36:32 -0400

    carts deletes line items instead of destroying self

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,8 +5,9 @@ class CartsController < ApplicationController
 
   def destroy
     @cart = @current_cart
-    @cart.destroy
-    session[:cart_id] = nil
-    redirect_to root_path
+    @cart.line_items.each do |item|
+      item.destroy
+    end
+    redirect_to cart_path(@cart.id)
   end
 end
