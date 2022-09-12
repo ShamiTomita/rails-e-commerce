9cda1fbd8576ae56d03f2f7f18f83beeafc48465
ShamiTomita <shamitomita@gmail.com>
2022-09-12 16:55:28 -0400

    devise roles for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
   has_many :line_items, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: [:user, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= "user"
+  end
 end
