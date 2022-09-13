7f6bd77b7f1448ceb6556b98465f2135a5a56258
ShamiTomita <shamitomita@gmail.com>
2022-09-13 19:01:28 -0400

    javascript

---
+++ app/javascript/controllers/cart_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="cart"
+export default class extends Controller {
+  static targets = ["price", "quantity", "total"];
+  connect() {
+    console.log("hola mis amores are you ready to see my")
+  }
+  //purely cosmetic, a quick fix for now
+  add(){
+    //quantity
+    let targetItem = this.quantityTarget.innerHTML
+    let resultQuantity = parseInt(targetItem) + 1
+    this.quantityTarget.innerHTML = resultQuantity;
+    let unitPrice = this.priceTarget.dataset.price;
+    let targetPrice = this.priceTarget.innerHTML
+    let resultPrice = parseFloat(targetPrice) + parseFloat(unitPrice)
+    this.priceTarget.innerHTML = resultPrice+'.0';
+  }
+  subtr(){
+    let targetItem = this.quantityTarget.innerHTML
+    let unitPrice = this.priceTarget.dataset.price;
+    let targetPrice = this.priceTarget.innerHTML
+    if(targetItem > 1){
+      let resultQuantity = parseInt(targetItem) - 1
+      this.quantityTarget.innerHTML = resultQuantity;
+      let resultPrice = parseFloat(targetPrice) - parseFloat(unitPrice)
+      this.priceTarget.innerHTML = resultPrice+'.0';
+      }
+
+  }
+
+  changeTotal(){
+    console.log(this.priceTarget)
+  }
+}
