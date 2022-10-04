c2b52cc7db1a4a27fb9e7e8eb842a1d9045b2f13
shamitomita <shamitomita@gmail.com>
2022-10-04 11:16:26 -0400

    line_items turbo stream

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,7 +3,8 @@ class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
   belongs_to :user
-
+  after_create_commit { broadcast_prepend_to "cart_items"}
+  after_update_commit {broadcast_replace_to "line_item"}
   def total_price
     tp = self.quantity * self.product.price
     tp.to_f
