e592b38fbd44176d4f0605f393d21ab4872528d6
shamitomita <shamitomita@gmail.com>
2022-10-07 19:15:15 -0400

    adding checkout items route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'carts/:id' => "carts#show", as: "cart"
   delete 'carts/:id' => "carts#destroy"
+
   get '/checkout', to: "orders#checkout"
+  get '/items', to: "orders#checkout_items"
   post 'line_items/:id/add' => "line_items#add_quantity", as: "line_item_add"
   post 'line_items/:id/reduce' => "line_items#reduce_quantity", as: "line_item_reduce"
   post 'line_items' => "line_items#create"
