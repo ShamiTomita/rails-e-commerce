2fb86690681ab7eb76cd05888bf17a16a2d58faa
shamitomita <shamitomita@gmail.com>
2022-10-13 21:10:11 -0400

    slideshow cont

--- app/javascript/controllers/slideshow_controller.js
+++ app/javascript/controllers/slideshow_controller.js
@@ -2,25 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [ "slide" ]
+  static values = { index: Number }
 
   initialize() {
-    this.index = 0
     this.showCurrentSlide()
   }
 
   next() {
-    this.index++
+    this.indexValue++
     this.showCurrentSlide()
   }
 
   previous() {
-    this.index--
+    this.indexValue--
     this.showCurrentSlide()
   }
 
   showCurrentSlide() {
     this.slideTargets.forEach((element, index) => {
-      element.hidden = index != this.index
+      element.hidden = index != this.indexValue
     })
   }
 }
