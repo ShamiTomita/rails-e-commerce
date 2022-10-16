49d64110e371b301070d4a46d02283c380df1799
shamitomita <shamitomita@gmail.com>
2022-10-16 17:41:58 -0400

    public controller

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,10 @@
 class PublicController < ApplicationController
   def index
     @products = Product.all
+    @array = []
+    while @array.length < 4
+      prod = @products.sample
+      @array.push(prod)
+    end
   end
 end
