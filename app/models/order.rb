eb5b8b292b987a767a993e85f76e5bec3c1ac831
shamitomita <shamitomita@gmail.com>
2022-10-18 14:36:53 -0400

    model associations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
+  belongs_to :cart
+  has_many :order_items
+
+  enum status: [:not_ordered, :order_submitted, :order_in_progress, :order_in_transit, :order_completed]
 end
