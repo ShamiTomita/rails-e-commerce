bab6744b3d20af772c4a9c97e844e380917c6cc4
shamitomita <shamitomita@gmail.com>
2022-10-19 15:29:39 -0400

    user controller update and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,19 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to profile_path
+  end
+
+  def show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :address)
+  end 
+
 
 end
