04dc487a7004dde8e8a9eab9b5fb1747575d9b05
shamitomita <shamitomita@gmail.com>
2022-11-01 15:45:56 -0400

    adding line item test

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe LineItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "must have a related cart" do
+    line_item = FactoryBot.create(:line_item)
+    expect(line_item.cart_id) != nil
+  end 
 end
