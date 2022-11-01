8e2c779339846076af5ab53305cf7fa1cbf0c8d3
shamitomita <shamitomita@gmail.com>
2022-11-01 15:55:19 -0400

    playing around with line item test

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe LineItem, type: :model do
   it "must have a related cart" do
+    cart = FactoryBot.create(:cart)
+    user = FactoryBot.create(:user)
+    product = FactoryBot.create(:product)
     line_item = FactoryBot.create(:line_item)
     expect(line_item.cart_id) != nil
-  end 
+    expect(line_item.user_id) != nil
+    expect(line_item.product_id) != nil
+  end
 end
