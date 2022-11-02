89ba8441e7db903d035830fabf110560f21605c2
shamitomita <shamitomita@gmail.com>
2022-11-02 11:20:11 -0400

    switch to using build inst of create for rspec factory bots

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe LineItem, type: :model do
-  it "must have a related cart" do
-    cart = FactoryBot.create(:cart)
-    user = FactoryBot.create(:user)
-    product = FactoryBot.create(:product)
-    line_item = FactoryBot.create(:line_item)
+  it "must have a related cart, user, and product" do
+    cart = FactoryBot.build(:cart)
+    user = FactoryBot.build(:user)
+    product = FactoryBot.build(:product)
+    line_item = FactoryBot.build(:line_item)
     expect(line_item.cart_id) != nil
     expect(line_item.user_id) != nil
     expect(line_item.product_id) != nil
