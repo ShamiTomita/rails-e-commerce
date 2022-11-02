ca74b90fbb1a19789219a6010e35e71f9c535115
shamitomita <shamitomita@gmail.com>
2022-11-02 16:58:38 -0400

    light methods and scope for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,11 @@ class Product < ApplicationRecord
   scope :filter_by_med_temp, -> {select {|p| p.med_temp === true}}
   scope :filter_by_high_temp, -> {select {|p| p.high_temp === true}}
 
+  scope :filter_by_low_light, -> {select {|p| p.low_light === true}}
+  scope :filter_by_med_light, -> {select {|p| p.med_light === true}}
+  scope :filter_by_high_light, -> {select {|p| p.high_light === true}}
+
+
   scope :low_to_high, -> {order(price: :asc)}
   scope :high_to_low, -> {order(price: :desc)}
   def self.ransackable_attributes(auth_object = nil)
@@ -63,6 +68,32 @@ class Product < ApplicationRecord
   def high_temp
     t = self.temp.split("-")
     if t[1].to_i >= 90
+      return true
+    else
+      false
+    end
+  end
+
+######light requirements
+
+  def low_light
+    if self.light.match(/shade/) || self.light.match(/tolerate/)
+      return true
+    else
+      false
+    end
+  end
+
+  def med_light
+    if self.light.match(/medium/) || self.light.match(/indirect/)
+      return true
+    else
+      false
+    end
+  end
+
+  def high_light
+    if self.light.match(/bright/)
       return true
     else
       false
