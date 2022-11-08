8d0a0d664dcc92360db9705c3880d8b62c45ba5b
shamitomita <shamitomita@gmail.com>
2022-11-08 16:30:38 -0500

    reformat

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,7 +2,7 @@ class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
   belongs_to :line_item
-  
+
   def total_price
     tp = self.quantity * self.product.price
     tp.to_f
