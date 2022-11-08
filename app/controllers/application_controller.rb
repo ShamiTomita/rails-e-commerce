b671ccbc7f66d9d056f637409e97c8f3c52020a5
shamitomita <shamitomita@gmail.com>
2022-11-08 10:52:15 -0500

    set stripe key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :current_cart
+  before_action :set_stripe_key
   private
     def current_cart
       if session[:cart_id]
@@ -19,5 +20,9 @@ class ApplicationController < ActionController::Base
         @current_cart = Cart.create
         session[:cart_id] = @current_cart.id
       end
+    end
+
+    def set_stripe_key
+      Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
     end
 end
