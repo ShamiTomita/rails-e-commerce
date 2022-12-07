ec75e96cecd8506b89270f5d2bbd98a60471c2eb
shamitomita <shamitomita@gmail.com>
2022-12-07 14:18:56 -0500

    revert

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,7 +2,7 @@ class PublicController < ApplicationController
   def index
     @products = Product.all
     @array = []
-    while @array.length < 12
+    while @array.length < 4
       prod = @products.sample
       @array.push(prod)
     end
