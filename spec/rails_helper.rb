315cae198c9e5f32b4508137af3496473ed6e2b1
shamitomita <shamitomita@gmail.com>
2022-12-19 18:44:21 -0500

    factory bot config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,10 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
+require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../config/environment", __dir__) # Prevent database truncation in production. Local? Try RAILS_ENV=test
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require "rspec/rails"
+
+Dir[Rails.root.join("spec", "support", "**", "*.rb")].sort.each { |f| require f }
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
