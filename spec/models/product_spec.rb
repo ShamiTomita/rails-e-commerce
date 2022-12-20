2da1203e5b8ef13f6b320b3b303ddbf98b2988d2
shamitomita <shamitomita@gmail.com>
2022-12-19 19:24:50 -0500

    product model validations spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  
+  describe "model validations" do 
+        it "requires a name" do 
+            plant = build(:product, name: "")
+            expect(plant.valid?).to be false 
+        end
+        it "requires a price" do 
+            plant = build(:product, price:"")
+            expect(plant.valid?).to be false 
+        end  
+    end 
 end
