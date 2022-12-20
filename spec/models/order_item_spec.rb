22328ef84209070ff2bfd04ca86706636ee81d8c
shamitomita <shamitomita@gmail.com>
2022-12-19 19:46:19 -0500

    order and order item validation specs

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
-  
+
+  describe "model validations" do
+    it "must have a related order, product, and line item" do
+      order = create(:order)
+      line_item = create(:line_item)
+      product = create(:product)
+      order_item = build(:order_item, order_id: order.id, line_item_id: line_item.id, product_id: product.id)
+      expect(order_item).to be_valid
+    end 
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
 
+  describe "model validations" do
+    it "must have a related cart and user" do
+      cart = create(:cart)
+      user = create(:user)
+      order = build(:order, cart_id: cart.id, user_id: user.id)
+      expect(order).to be_valid
+    end 
+  end
 end
