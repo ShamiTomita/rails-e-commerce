b167172cafa56887e5f1e5ed337e0958002f1561
shamitomita <shamitomita@gmail.com>
2023-01-10 11:25:03 -0500

    removing and renaming

--- spec/system/filters_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "Filters", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
-####This is to test out product filtering??
-  pending "add some scenarios (or delete) #{__FILE__}"
-end
