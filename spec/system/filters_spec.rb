e640624fd6a81a7870176b5c933446667e2bba96
shamitomita <shamitomita@gmail.com>
2023-01-09 22:18:31 -0500

    filters spec

---
+++ spec/system/filters_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "Filters", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+####This is to test out product filtering??
+  pending "add some scenarios (or delete) #{__FILE__}"
+end
