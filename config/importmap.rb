a5c1fd0537ee0ae358499de697f357617ba93f74
SHAMI_TOMITA <shamitomita@gmail.com>
2023-05-28 16:18:15 -0400

    updates

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,6 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "chartkick", to: "chartkick.js"
+pin "Chart.bundle", to: "Chart.bundle.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
