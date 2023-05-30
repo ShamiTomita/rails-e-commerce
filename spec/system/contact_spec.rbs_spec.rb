d3805281eefc6ede5d6fc75c62676d0a45a159c3
SHAMI_TOMITA <shamitomita@gmail.com>
2023-05-30 09:25:38 -0400

    contact spec file

---
+++ spec/system/contact_spec.rbs_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "ContactSpec.rbs", type: :system do
+  #what does this do? It lets users send contact messages to the application. 
+  #It is a flexible message taker i.e. it doesn't expect the user to not know minute details 
+  
+end
